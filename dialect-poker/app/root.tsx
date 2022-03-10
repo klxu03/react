ac88925c204954c160aa04005dce94d392d69db6
klxu03 <beaver2025@gmail.com>
2022-03-10 18:26:52 -0500

    Set up layout for Dialect Poker Subsidiary

--- dialect-poker/app/root.tsx
+++ dialect-poker/app/root.tsx
@@ -14,20 +14,11 @@ export const meta: MetaFunction = () => {
 
 export default function App() {
 	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
+		<Document title="Dialect Poker Subsidiary">
+			<Layout>
 				<Outlet />
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-			</body>
-		</html>
+			</Layout>
+		</Document>
 	);
 }
 
@@ -40,6 +31,8 @@ const Document = ({ children, title }: DocumentType) => {
 	return (
 		<html lang="en">
 			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<Meta />
 				<Links />
 				<title>{title ? title : "My Custom Default Title"}</title>
@@ -49,7 +42,17 @@ const Document = ({ children, title }: DocumentType) => {
 				{process.env.NODE_ENV === "development" && <LiveReload />}
 				<Scripts />
 				<ScrollRestoration />
+				<LiveReload />
 			</body>
 		</html>
+	);
+};
+
+const Layout = ({ children }: any) => {
+	return (
+		<>
+			<div>Layout</div>
+			{children}
+		</>
 	);
 };
