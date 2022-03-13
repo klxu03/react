cd9978c1d4d77ad78bac6d8dd9a171edc3eb780f
klxu03 <beaver2025@gmail.com>
2022-03-12 22:22:47 -0500

    Add Round and Dashboard frontend Fleshed Out

--- dialect-caddy/app/components/Sidebar.tsx
+++ dialect-caddy/app/components/Sidebar.tsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+
 export function Sidebar() {
-	return (
-		<div className="sidebar-container">
-			<div className="sidebar-wrapper">
-				This is a sidebar
-				<div className="menu-container">
-					<div className="menu-item">Home</div>
-					<div className="menu-item menu-item-active">Caddies</div>
-					<div className="menu-item">Help</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [active, setActive] = useState<string>('home');
+
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-wrapper">
+        This is a sidebar
+        <div className="menu-container">
+          <div
+            className={
+              'menu-item ' + (active == 'home' ? 'menu-item-active' : '')
+            }
+            onClick={() => setActive('home')}
+          >
+            Home
+          </div>
+          <div
+            className={
+              'menu-item ' + (active == 'caddies' && 'menu-item-active')
+            }
+            onClick={() => setActive('caddies')}
+          >
+            Caddies
+          </div>
+          <div
+            className={'menu-item ' + (active == 'help' && 'menu-item-active')}
+            onClick={() => setActive('help')}
+          >
+            Help
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- dialect-caddy/app/root.tsx
+++ dialect-caddy/app/root.tsx
@@ -1,62 +1,62 @@
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+
+import globalStylesUrl from '~/styles/global.css';
 
 export const meta: MetaFunction = () => {
-	return { title: "Caddy" };
+  return { title: 'Dialect CaddyShack Subsidiary' };
 };
 
-import globalStylesUrl from "~/styles/global.css";
-
 export const links = () => [
-	{
-		rel: "stylesheet",
-		href: globalStylesUrl,
-	},
+  {
+    rel: 'stylesheet',
+    href: globalStylesUrl,
+  },
 ];
 
 export default function App() {
-	return (
-		<Document title="Dialect CaddyShack Subsidiary">
-			<Layout>
-				<Outlet />
-			</Layout>
-		</Document>
-	);
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
 }
 
 type DocumentType = {
-	children: any;
-	title: string;
+  children: any;
+  //   title: string;
 };
 
-const Document = ({ children, title }: DocumentType) => {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<Meta />
-				<Links />
-				<title>{title ? title : "My Custom Default Title"}</title>
-			</head>
-			<body>
-				{children}
-				{process.env.NODE_ENV === "development" && <LiveReload />}
-				<Scripts />
-				<ScrollRestoration />
-				<LiveReload />
-			</body>
-		</html>
-	);
+const Document = ({ children }: DocumentType) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        {/* <title>{title ? title : 'My Custom Default Title'}</title> */}
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+        <Scripts />
+        <ScrollRestoration />
+        <LiveReload />
+      </body>
+    </html>
+  );
 };
 
 const Layout = ({ children }: any) => {
-	return <>{children}</>;
+  return <>{children}</>;
 };
